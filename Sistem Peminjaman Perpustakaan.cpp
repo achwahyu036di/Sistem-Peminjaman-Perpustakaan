f0f08e5fa96c5252c29466d68638dd32bcd1f29f
Amru Muiz Fauzan <163871761+LordZerav@users.noreply.github.com>
2024-06-23 00:02:56 +0700

    Tugas Amru
    Update

--- Sistem Peminjaman Perpustakaan.cpp
+++ Sistem Peminjaman Perpustakaan.cpp
@@ -5,34 +5,55 @@
 
 using namespace std;
 
-struct buku {
-    string judul_buku;
-    string nama_pengarang;
-    int kode_buku;
+class Buku {
+private:
+    string judulBuku, pengarangBuku;
+    int tahunTerbit, stokBuku;
+    bool statusBuku;
+public:
+    Buku () {}
+    Buku (string judul, string pengarang, int tahun, int stok, bool status) {
+        judulBuku = judul;
+        pengarangBuku = pengarang;
+        tahunTerbit = tahun;
+        stokBuku = stok;
+        statusBuku = status;
+    }
 }
 
-struct peminjam {
-    string nama_peminjam;
-    int id_peminjam;
+class Peminjam {
+private:
+    string namaPeminjam, alamatPeminjam, nomorTelponPeminjam;
+public:
+    Peminjam () {}
+    Peminjam (string nama, string alamat, string nomor) {
+        namaPeminjam = nama;
+        alamatPeminjam = alamat;
+        nomorTeleponPeminjam = nomor;
+    }
 }
 
-void buku() {
+Buku daftarBuku[100];
+
+int n;
+
+int main () {
+    cout << ":::::::::::::::: SELAMAT DATANG DI PERPUSTAKAAN PUTRA BANGSA ::::::::::::::::\n";
+    cout << "-----------------------------------------------------------------------------\n";
+
+    cout << "Silahkan Anda bisa memilih layanan berikut ini :\n";
+    cout << "[1] Menambahkan Buku Baru \n";
+    cout << "[2] Mencari Buku\n";
+    cout << "[3] Mengedit Informasi Buku\n";
+    cout << "[4] Meminjam Buku\n";
+    cout << "[5] Mengembalikan Buku\n";
+    cout << "[6] Menampilkan Daftar Buku\n";
+    cout << "[7] Keluar\n";
+    cout << "\n";
     
+    cout << "Silahkan tentukan pilihan Anda [1] - [7]\n";
+    cin >> n ;
 
-}
-
-int a;
-
-int main() {
-    cout << "::: SISTEM PEMINJAMAN PERPUSTAKAAN :::\n";
-    cout << "--------------------------------------\n";
     
-    cout << "Masukkan Pilihan Anda : " << endl;
-    cin >> a;
-
-    cout << "[1] Input Data Buku \n";
-    cout << "[2] Daftar Buku\n";
-    cout << "[3] Input Data Peminjam\n";
-    cout << "[4] Daftar Peminjam\n";
-    
+    return 0;
 }
